dcd035815c1dc97df52288c4ae9c367e5b839d58
procopio420 <lucas-procopio@outlook.com>
2020-05-15 01:09:13 -0300

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const db = require('./queries');
 
 app.use(express.json({ limit: '50mb' }));
-app.use(cors());
 
 app.get('/:repository', db.getDuplas);
 app.post('/:repository', db.createDupla);
@@ -13,3 +11,4 @@ app.post('/create/:repository', db.createTable);
 app.listen(8000, () => {
   console.log('Server started!');
 });
+
