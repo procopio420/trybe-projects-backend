379e5c25098febcb0081f3f688dc17e79af622dc
procopio420 <lucas-procopio@outlook.com>
2020-05-15 02:34:53 -0300

    configuring heroku port and scripts

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const db = require('./queries');
 
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/:repository', db.getDuplas);
 app.post('/:repository', db.createDupla);
 app.post('/create/:repository', db.createTable);
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server started!');
 });
-
